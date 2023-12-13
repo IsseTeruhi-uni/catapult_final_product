126a103626d78c55cf5f3600ecebe5b2e7104605
tsunakit99 <tainosuke0926@gmail.com>
2023-12-13 20:03:34 +0900

    groupテーブル追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Hobby::class)->withTimeStamps();
     }
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
 }
