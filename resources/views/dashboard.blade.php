f83e905571635f9a714587a8c151cf6c668b01e4
teruterubouzu <131101441+IsseTeruhi-uni@users.noreply.github.com>
2023-12-24 19:51:51 +0900

    Revert "Feat/#6 add create simple qrcode"

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,10 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    {{-- 変更ここから --}}
+
+                    @can('register')
+                    <p>register権限を持つ人だけが見れるよ</p>
+                    @endcan
+                    <p>みんな見れるよ</p>
+
+                    {{-- ここまで --}}
                 </div>
             </div>
         </div>
     </div>
-    
 </x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,35 +1,45 @@
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
-  <!-- Primary Navigation Menu -->
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <div class="flex justify-between h-16">
-      <div class="flex">
-        <!-- Logo -->
-        <div class="shrink-0 flex items-center">
-          <a href="{{ route('dashboard') }}">
-            <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-          </a>
-        </div>
+    <!-- Primary Navigation Menu -->
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-16">
+            <div class="flex">
+                <!-- Logo -->
+                <div class="shrink-0 flex items-center">
+                    <a href="{{ route('dashboard') }}">
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    </a>
+                </div>
+                
+                <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __(' Dashboard ') }}
+                    </x-nav-link>
+                </div>
 
-        <!-- Navigation Links -->
-        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-          <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-            {{ __('Dashboard') }}
-          </x-nav-link>
-        </div>
-        <!-- 🔽 一覧ページへのリンクを追加 -->
-        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-          <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
-            {{ __('Home') }}
-          </x-nav-link>
-        </div>
-      </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('employees.show',Auth::user()->id)" :active="request()->routeIs('employees.show')">
+                        {{ __('MyProfile') }}
+                    </x-nav-link>
+                </div>
+                @can('register')
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="url('/admin/auth/login')" :active="request()->routeIs('admin.login')">
+                        管理画面
+                    </x-nav-link>
+                </div>
+                @endcan
+            </div>
+            <!-- Settings Dropdown -->
+            <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <div class="mr-3">
+                    <img id="preview1" src="{{ isset(Auth::user()->profile_photo_path) ? asset('storage/' . Auth::user()->profile_photo_path) : asset('images/user_icon.png') }}" alt="" class="w-16 h-16 rounded-full object-cover border-none bg-gray-200" style="width: 45px; height: 45px;">
+                </div>
 
-      <!-- Settings Dropdown -->
-      <div class="hidden sm:flex sm:items-center sm:ml-6">
-        <x-dropdown align="right" width="48">
-          <x-slot name="trigger">
-            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-              <div>{{ Auth::user()->name }}</div>
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                            <div>{{ Auth::user()->name }}</div>
 
               <div class="ml-1">
                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -48,7 +58,8 @@
             <form method="POST" action="{{ route('logout') }}">
               @csrf
 
-              <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+                            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+
                                                 this.closest('form').submit();">
                 {{ __('Log Out') }}
               </x-dropdown-link>
@@ -105,7 +116,8 @@
         <form method="POST" action="{{ route('logout') }}">
           @csrf
 
-          <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
+                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
+
                                         this.closest('form').submit();">
             {{ __('Log Out') }}
           </x-responsive-nav-link>
@@ -114,4 +126,3 @@
     </div>
   </div>
 </nav>
-
