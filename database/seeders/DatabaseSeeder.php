46b4ea028b61949bb37a18470309f100a88acb87
IsseTeruhi-uni <isse.teruhi220@mail.kyutech.jp>
2024-01-11 14:57:01 +0900

    fix:vite.config.js

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(CompanyTableSeeder::class);
+        $this->call(GroupTableSeeder::class);
+        $this->call(HobbyTableSeeder::class);
+        $this->call(PostTableSeeder::class);
+        $this->call(SkillSeeder::class);
         $this->call(MasterDatabaseSeeder::class);   
     }
 }
